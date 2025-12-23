e6f317af9cf3dab07f1b85258acf5bc1ef2abb12
Abdullah almousa <abdullahmlwork@gmail.com>
2025-12-24 01:32:20 +0300

    Add theme

--- composeApp/src/commonMain/kotlin/com/mysticbyte/permissionhandling/App.kt
+++ composeApp/src/commonMain/kotlin/com/mysticbyte/permissionhandling/App.kt
@@ -1,16 +1,23 @@
 package com.mysticbyte.permissionhandling
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.mysticbyte.permissionhandling.theme.IdTextApp
 import com.mysticbyte.permissionhandling.viewmodel.PermissionsViewModel
 import dev.icerock.moko.permissions.PermissionState
 import dev.icerock.moko.permissions.compose.BindEffect
@@ -21,7 +28,11 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
 
-    Box(){
+    Box(
+        modifier = Modifier
+            .background(Color(0xFF232323))
+            .fillMaxSize()
+    ){
 
         val factory = rememberPermissionsControllerFactory()
         val controller = remember(factory){
@@ -34,37 +45,57 @@ fun App() {
             PermissionsViewModel(controller)
         }
 
-        Column(
-
+        Box(
             modifier = Modifier
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxSize()
+                .padding(16.dp)
+        ){
 
-        ) {
+            Column(
 
-            when(viewModel.state) {
-                PermissionState.Granted -> {
-                    Text("Record Audio Permission Granted!")
-                }
-                PermissionState.DeniedAlways -> {
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
 
-                    Text("Permission Was Permanently Declined!")
-                    Button(onClick = {
+            ) {
 
-                        controller.openAppSettings()
+                when(viewModel.state) {
+                    PermissionState.Granted -> {
+                        Text("Record Audio Permission Granted!")
+                    }
+                    PermissionState.DeniedAlways -> {
 
-                    }){
-                        Text("Open App Settings")
+                        Text("Permission Was Permanently Declined!")
+                        Button(onClick = {
+
+                            controller.openAppSettings()
+
+                        }){
+                            Text("Open App Settings")
+                        }
+                    }
+                    else -> {
+                        Button(onClick = {
+                            viewModel.provideOrRequestAudioPermissions()
+                        }){
+                            Text("Request Permission")
+                        }
                     }
                 }
-                else -> {
-                    Button(onClick = {
-                        viewModel.provideOrRequestAudioPermissions()
-                    }){
-                        Text("Request Permission")
-                    }
-                }
+
+            }
+
+            Column(
+                modifier = Modifier.align(Alignment.BottomCenter)
+            ) {
+                Text(
+                    text = IdTextApp.idTextApp,
+                    fontSize = 12.sp,
+                    color = Color(0xFF9A9A9A),
+                    modifier = Modifier.padding(6.dp),
+                    textDecoration = TextDecoration.Underline
+                )
             }
 
         }
---
+++ composeApp/src/commonMain/kotlin/com/mysticbyte/permissionhandling/theme/IdTextApp.kt
@@ -0,0 +1,7 @@
+package com.mysticbyte.permissionhandling.theme
+
+object IdTextApp {
+
+    val idTextApp = "Made By ©Abdullah AL-Mousa"
+
+}
